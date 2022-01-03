0cd4e1c300fa24a56e4bea585b25fa38b122f3eb
Kevin Cutler <39454982+kevinjohncutler@users.noreply.github.com>
2022-01-03 02:50:36 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ncolor
+Fast remapping of intance labels 1,2,3,...,M to N repeating, disjoint labels, 1,2,3,...,N. The four-color-theorem guarantees that at most four colors is required for any 2D segmentation/map, but this algorithm will opt for 5 or 6 to give an acceptable result quickly. Also works for 3D labels (&lt;8 colors typically required) and perhaps higher dimensions as well.
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from setuptools import setup
+
+install_deps = ['numpy>=1.20.0', 'scipy', 'numba', 
+                'edt','fastremap','scikit-image']
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name="ncolor",
+    license="BSD",
+    author="Kevin Cutler",
+    author_email="kevinjohncutler@outlook.com",
+    description="label matrix coloring algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kevinjohncutler/ncolor",
+    setup_requires=[
+      'pytest-runner',
+      'setuptools_scm',
+    ],
+    packages=setuptools.find_packages(),
+    use_scm_version=True,
+    install_requires = install_deps,
+    # tests_require=[
+    #   'pytest'
+    # ],
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    )
+)
