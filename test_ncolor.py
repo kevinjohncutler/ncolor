02dbf691c4fc6185a0d25dd87725cf2b5415959c
Kevin Cutler <39454982+kevinjohncutler@users.noreply.github.com>
2022-01-03 17:54:55 -0800

    Add tracking to test files.

---
+++ test_ncolor.py
@@ -0,0 +1,11 @@
+import pytest
+import os
+from pathlib import Path
+import skimage.io
+import ncolor.ncolor as ncolor
+
+
+def test_ncolor():
+    masks_dir = Path(os.path.dirname(ncolor.__file__)).parent.absolute()
+    masks = skimage.io.imread(os.path.join(masks_dir,'example.png'))
+    ncolor_masks = ncolor.label(masks)
