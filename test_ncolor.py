ff70d6380b5e349e9d6d3df39a690b87dd6a732b
Kevin Cutler <kevinjohncutler@outlook.com>
2025-03-10 17:57:02 -0400

    update test file dir

--- test_ncolor.py
+++ test_ncolor.py
@@ -1,11 +1,13 @@
-import pytest
 import os
 from pathlib import Path
 import skimage.io
 import ncolor
 
-
 def test_ncolor():
-    masks_dir = Path(os.path.dirname(ncolor.__file__)).parent.absolute()
+    masks_dir = Path(os.path.dirname(ncolor.__file__)).parent.parent.absolute()
+    print('masks dir', masks_dir)
     masks = skimage.io.imread(os.path.join(masks_dir,'example.png'))
     ncolor_masks = ncolor.label(masks)
+    
+if __name__ == '__main__':
+    test_ncolor()
