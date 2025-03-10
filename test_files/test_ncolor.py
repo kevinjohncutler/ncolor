af1bf72f3635baaf790de4770a4541cbf95a6098
Kevin Cutler <kevinjohncutler@outlook.com>
2025-03-10 18:22:31 -0400

    tox.ini

--- test_files/test_ncolor.py
+++ test_files/test_ncolor.py
@@ -4,8 +4,9 @@ import skimage.io
 import ncolor
 
 def test_ncolor():
-    rootdir = Path(ncolor.__file__).parent.parent.parent
-    masks_dir = os.path.join(rootdir,'test_files')
+    # rootdir = Path(ncolor.__file__).parent.parent.parent
+    # masks_dir = os.path.join(rootdir,'test_files')
+    masks_dir = os.path.dirname(os.path.abspath(__file__))
     print('masks dir', masks_dir)
     masks = skimage.io.imread(os.path.join(masks_dir,'example.png'))
     ncolor_masks = ncolor.label(masks)
